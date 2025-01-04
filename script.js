e6046a4aa923653afca38796f682fe48ed65534e
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-05 01:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function PromiseCreation(url){
+	return new Promise((resolve, reject) => {
+		let img = new Image();
+
+		img.onload = function(){
+			return resolve(img);
+		}
+
+		img.onerror = function(){
+			return reject(new Error(`Failed to load image's URL: ${url}`));
+		}
+
+		img.src=url;
+	});
+};
+
+btn.addEventListener('click', function(){
+	let promisesArr = images.map((imgUrl) => PromiseCreation(imgUrl.url));//map returns the output to prmoises
+
+	Promise.all(promisesArr).then((images) => {
+		images.forEach((img) => {
+			output.appendChild(img);
+		})
+	})
+	.catch((error)=>{
+			console.log(error);
+	})
+});
